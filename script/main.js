d79863ef262868c22d7d75cf13e7bc4e71a1f12d
Artur Frolovnin <96828393+Artur78r@users.noreply.github.com>
2023-03-12 18:41:57 +0300

    update

--- script/main.js
+++ script/main.js
@@ -7,24 +7,23 @@ window.onload = function () {
     const AvatarImg = document.getElementById('background-avatar');
     let isCheckboxChecked = false;
 
-    const arrayWord = ["Очень интересно", 
-    "Расскажи еще что нибудь", 
-    "Продолжай не останавливайся", 
-    "Ты прирожден писать, ударение поставь сам", 
-    "Рад видеть твои сообщения", 
-    "Вот как", 
-    "Поговорим", 
-    "Совсем тебе скучно", 
-    "Даже не знаю, что на это ответить", 
-    "Понятно", 
-    "Ясно", 
-    "хаха"];
+    const arrayWord = ["Очень интересно",
+        "Расскажи еще что нибудь",
+        "Продолжай не останавливайся",
+        "Ты прирожден писать, ударение поставь сам",
+        "Рад видеть твои сообщения",
+        "Вот как",
+        "Поговорим",
+        "Совсем тебе скучно",
+        "Даже не знаю, что на это ответить",
+        "Понятно",
+        "Ясно",
+        "хаха"];
 
     const classNamesMessages = {
-        USER_TEXT: "user-text",
-        USER_TIMESTAMP: "user-timestamp",
-        BOT_TEXT: "bot-text",
-        BOT_TIMESTAMP: "bot-timestamp"
+        USER_MESSAGES: "user-messages",
+        BOT_MESSAGES: "bot-messages",
+        TIMESTAMP: "timestamp"
     }
 
     const stateAttribute = {
@@ -46,63 +45,33 @@ window.onload = function () {
         return document.createElement("div");
     }
 
-    function createUserMessage() {
+    function createMessage() {
         const messageContainer = createDiv();
-        messageContainer.className = classNamesMessages.USER_TEXT;
-        messageContainer.innerHTML = inputMessage.value;
-        appendTimestampToUserMessage(messageContainer);
+        if (isCheckboxChecked) {
+            messageContainer.className = classNamesMessages.BOT_MESSAGES;
+            messageContainer.innerHTML = selectRandomWord();
+        }
+        else {
+            messageContainer.className = classNamesMessages.USER_MESSAGES;
+            messageContainer.innerHTML = inputMessage.value;
+        }
+        appendTimestamp(messageContainer);
         return messageContainer;
     }
 
-    function appendTimestampToUserMessage(message) {
+    function appendTimestamp(message) {
         const timestampContainer = createDiv();
-        timestampContainer.className = classNamesMessages.USER_TIMESTAMP;
+        timestampContainer.className = classNamesMessages.TIMESTAMP;
         timestampContainer.innerHTML = createTimestamp();
         message.append(timestampContainer);
     }
 
-    function sendUserMessage() {
-        chat.append(createUserMessage());
+    function sendMessage() {
+        chat.append(createMessage());
     }
-
-    function createBotMessage() {
-        const messageContainer = createDiv();
-        messageContainer.className = classNamesMessages.BOT_TEXT;
-        messageContainer.innerHTML = selectRandomWord();
-        appendTimestampToBotMessage(messageContainer);
-        return messageContainer;
-    }
-
-    function appendTimestampToBotMessage(message) {
-        const timestamp = createDiv();
-        timestamp.className = classNamesMessages.BOT_TIMESTAMP;
-        timestamp.innerHTML = createTimestamp();
-        message.append(timestamp);
-    }
-
-    function sendBotMessage() {
-        chat.append(createBotMessage());
-    }
-
-    function scrollpage() {
-        chat.scrollBy({ top: 1000, left: 0, behavior: 'smooth' });
-    }
-
-    function isInputEmpty() {
-        if (inputMessage.value === "") {
-            buttonSendMessage.setAttribute(stateAttribute.DISABLED, true);
-        }
-        else {
-            buttonSendMessage.removeAttribute(stateAttribute.DISABLED);
-        }
-    }
-
-    function clearInput() {
-        inputMessage.value = "";
-    };
 
     function switchAvatar() {
-        if (isCheckboxChecked === true) {
+        if (isCheckboxChecked) {
 
             AvatarImg.classList.remove(objectWitchClassName.INACTIVE);
         }
@@ -111,22 +80,44 @@ window.onload = function () {
         }
     }
 
+    function isInputEmpty() {
+        return inputMessage.value === "";
+    }
+
+    function disabletButtonIfInputEmpty() {
+        if (isInputEmpty()) {
+            buttonSendMessage.setAttribute(stateAttribute.DISABLED, true);
+        }
+        else {
+            buttonSendMessage.removeAttribute(stateAttribute.DISABLED);
+        }
+    }
+
+    function scrollChat() {
+        chat.scrollBy({ top: 1000, left: 0, behavior: 'smooth' });
+    }
+
+    function clearInput() {
+        inputMessage.value = "";
+    }
+
+    function resetInput() {
+        clearInput();
+        disabletButtonIfInputEmpty()
+    }
+
     botSwitch.onclick = function () {
         isCheckboxChecked = botSwitch.checked
         switchAvatar();
     }
 
     inputMessage.oninput = function () {
-        isInputEmpty();
-    };
+        disabletButtonIfInputEmpty()
+    }
 
     buttonSendMessage.onclick = function () {
-        sendUserMessage();
-        if (isCheckboxChecked) {
-            sendBotMessage();
-        }
-        clearInput();
-        isInputEmpty();
-        scrollpage();
+        sendMessage();
+        resetInput()
+        scrollChat();
     }
 }
